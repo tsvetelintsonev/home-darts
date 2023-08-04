967a45aa23e01402e480cbe63b93fcfd899ebe01
Tsvetelin Tsonev <tsvetelin.tsoneff.github@gmail.com>
2023-08-04 12:47:36 +0200

    refactor(domain): Move DartPoints Value assignment in ctor
    1. fix: (bug fix)
    2. feat: (new feature)
    3. BREAKING CHANGE:
    4. build:
    5. chore: (miscellaneous commits e.g. modifying .gitignore)
    6. ci: (changes to ci/cd pipelines)
    7. docs: (documentation only)
    8. style: (white-space, formatting, missing semi-colons, etc)
    9. refactor: (refactoring code)
    10. perf: (improve performance)
    11. test: (new tests, correcting existing tests)

--- src/HomeDarts.Domain/DartPoints.cs
+++ src/HomeDarts.Domain/DartPoints.cs
@@ -2,28 +2,10 @@ namespace HomeDarts.Domain;
 
 public class DartPoints
 {
-    private int? _value;
-
-    public DartPoints(string label) => Label = label;
-    
-    public string Label { get; }
-    public int Value {
-        get
-        {
-            if (_value is not null) return (int)_value;
-
-            _value = GetValue();
-
-            return (int)_value;
-        }
-    }
-
-    public bool IsDouble() => Label[0] == 'D';
-    
-    public DartPoints Copy() => new (Label);
-
-    private int GetValue()
-        => Label switch
+    public DartPoints(string label)
+    {
+        Label = label;
+        Value = Label switch
         {
             "BULL" => 50,
             _ when Label[0] == 'T' => ConvertFromLabelWithPrefix() * 3,
@@ -31,7 +13,14 @@ public class DartPoints
             _ when Label[0] == 'S' => ConvertFromLabelWithPrefix(),
             _ => Convert.ToInt32(Label),
         };
+    }
+
+    public string Label { get; }
+    public int Value { get; }
+
+    public bool IsDouble() => Label[0] == 'D';
     
+    public DartPoints Copy() => new (Label);
+
     private int ConvertFromLabelWithPrefix() => Convert.ToInt32(Label[1..]);
-    
 }
